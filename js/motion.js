6689a6514ca807b08a9eac9701b1162e3b729130
topdayplus <2513381547@qq.com>
2022-11-06 22:38:29 +0800

    Site updated: 2022-11-06 22:38:29

--- js/motion.js
+++ js/motion.js
@@ -18,7 +18,7 @@ NexT.motion.integrator = {
     if (!CONFIG.motion.async) this.queue = [this.queue];
     this.queue.forEach(sequence => {
       const timeline = window.anime.timeline({
-        duration: 200,
+        duration: 120,
         easing  : 'linear'
       });
       sequence.forEach(item => {
@@ -37,7 +37,7 @@ NexT.motion.middleWares = {
       sequence.push({
         targets,
         scaleX  : [0, 1],
-        duration: 500,
+        duration: 120,
         deltaT  : '-=200'
       });
     }
